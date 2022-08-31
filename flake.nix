274256ed0556c63c28391c7949789cc2cf017226
Bouke van der Bijl <i@bou.ke>
2022-08-31 10:40:08 +0200

    Make it an overlay

--- flake.nix
+++ flake.nix
@@ -1,18 +1,16 @@
 {
   description = "Cross-compilation toolchain for nix";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { ... }:
     let
       targets = [ "arm-linux-gnueabihf" ];
-      pkgs = nixpkgs.legacyPackages.${system};
     in
       {
-        packages = builtins.foldl' (a: b: a // b) {} (map (target: {
-          "${target}-binutils" = pkgs.callPackage ./binutils.nix { inherit target; };
-        }) targets);
-      }
-    );
+        overlay = (_: super: 
+          builtins.listToAttrs (map (target: {
+            name = "${target}-binutils";
+            value = super.callPackage ./binutils.nix { inherit target; };
+          }) targets)
+        );
+      };
 }
